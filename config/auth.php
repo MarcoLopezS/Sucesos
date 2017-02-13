28eacf2e83c2cd43f7a012060ba0bbbe9afb1a12
Marco Lopez <mlopez@marostdevelopers.com>
2017-02-13 11:16:22 -0500

    config auth

--- config/auth.php
+++ config/auth.php
@@ -67,7 +67,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => Sucesos\User::class,
+            'model' => Sucesos\Entities\User::class,
         ],
 
         // 'users' => [
