d3896ade367572f75c97d49bb88b37454197463f
Marco Lopez <mlopez@marostdevelopers.com>
2017-02-13 13:13:39 -0500

    portada en home

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -193,6 +193,17 @@
                     </ul>
                     <div class="clearfix"></div>
                 </div>
+
+                <div class="side-widget margin-bottom-60">
+                    <div class="side-widget margin-bottom-0">
+                        <h3 class="heading-1"><span>Portada</span></h3>
+                        <div class="layout_3--item">
+                            <div class="thumb">
+                                <a href="#"><img src="http://cdn.expansion.mx/dims4/default/ddffee9/2147483647/thumbnail/400x517%5E/quality/75/?url=http%3A%2F%2Fcdn.expansion.mx%2Fc7%2F2a%2F0a23dbd04635966bf78433c8a68a%2Fexp-1205-portada.jpg" class="img-responsive" alt=""></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </aside>
         </div>
     </div>
