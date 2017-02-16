24ff57691878ff90bc1f40d049dcc44b7abca975
Marco Lopez <mlopez@marostdevelopers.com>
2017-02-15 19:11:47 -0500

    funciones en helpers

--- app/helpers.php
+++ app/helpers.php
@@ -51,3 +51,166 @@ function fechaPubAdmin($datetime)
     $fecha = $fecha->format('d/m/Y H:i');
     return ucfirst($fecha);
 }
+
+function fechaPublicacionBD($datetime)
+{
+    $fecha = Date::createFromFormat('d/m/Y H:i:s', $datetime);
+    $fecha = $fecha->format('Y-m-d H:i');
+    return ucfirst($fecha);
+}
+
+//UPLOAD DE ARCHIVO
+/**
+ * @param $type = Tipo de archivo: Documentos o Upload (imagen)
+ * @param $file = Archivo
+ * @return mixed
+ */
+function UploadFile($type, $file)
+{
+    CrearCarpeta($type);
+    $path = $type."/".FechaCarpeta();
+    $upload = FileMove($file, $path);
+    $carpeta = FechaCarpeta();
+
+    $archivo['nombre'] = $upload['original'];
+    $archivo['extension'] = $upload['extension'];
+    $archivo['archivo'] = $upload['archivo'];
+    $archivo['carpeta'] = $carpeta;
+    $archivo['size'] = $upload['size'];
+
+    return $archivo;
+}
+
+/*
+ * Upload de Logo
+ */
+function UploadLogo($type, $file)
+{
+    CrearCarpeta($type);
+    $path = $type."/";
+    $upload = FileMove($file, $path);
+
+    $archivo['nombre'] = $upload['original'];
+    $archivo['extension'] = $upload['extension'];
+    $archivo['archivo'] = $upload['archivo'];
+    $archivo['size'] = $upload['size'];
+
+    return $archivo;
+}
+
+//MOVER ARCHIVO
+function FileMove($file, $path)
+{
+    $name = $file->getClientOriginalName();                 //NOMBRE Y EXTENSION DE ARCHIVO
+    $extension = $file->getClientOriginalExtension();       //EXTENSION DE ARCHIVO
+    $size = $file->getSize();
+    $archive = basename($name, ".".$extension);             //NOMBRE DE ARCHIVO
+    $pathName = SlugUrl($archive)."-".date('YmdHi'); //CONVERTIR NOMBRE SIN ESPACIOS
+    $filename = $pathName.".".$extension;                   //NOMBRE Y EXTENSION SIN ESPACIOS
+    $file->move($path, $filename);                          //MOVER ARCHIVO A NUEVA CARPETA
+
+    $archivo['original'] = $archive;                        //DEVOLVER SOLO NOMBRE ORIGINAL
+    $archivo['extension'] = $extension;                     //DEVOLVER SOLO EXTENSION
+    $archivo['archivo'] = $filename;                        //DEVOLVER NOMBRE CON EXTENSION
+    $archivo['size'] = $size;                               //DEVOLVER EL TAMAÑO DEL ARCHIVO
+
+    return $archivo;
+}
+
+//CARPETA CON NOMBRE DEL MES ACTUAL
+function FechaCarpeta()
+{
+    $meses = array("enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre");
+    $mes = date('n')-1; // devuelve el número del mes
+    $ano = date('Y'); // devuelve el año
+    $fechaCarpeta = $meses[$mes]."".$ano."/";
+    return $fechaCarpeta;
+}
+
+/* CREACION DE CARPETA */
+function CrearCarpeta($type){
+    $nombre_carpeta = FechaCarpeta();
+    $ruta = public_path($type."/".$nombre_carpeta);
+    if(!is_dir($ruta)){
+        @mkdir($ruta, 0755);
+        $carpeta=$ruta;
+    }else{
+        $carpeta=$ruta;
+    }
+    return $carpeta;
+}
+
+/* URL AMIGABLE */
+function SlugUrl($texto){
+    return getUrlAmigable(eliminarTextoURL($texto));
+}
+
+function eliminarTextoURL($str) {
+    $search = array('&lt;', '&gt;', '&quot;', '&amp;');
+    $str = str_replace($search, '', $str);
+    $search = array('&aacute;','&Aacute;','&eacute;','&Eacute;','&iacute;','&Iacute;','&oacute;','&Oacute;','&uacute;','&Uacute;','&ntilde;','&Ntilde;');
+    $replace = array('a','a','e','e','i','i','o','o','u','u','n','n');
+    $search = array('Á', 'É', 'Í', 'Ó', 'Ú', 'á', 'é', 'í', 'ó', 'ú', 'Ü', 'ü', 'Ñ', 'ñ', '_', '-');
+    $replace = array('a', 'e', 'i', 'o', 'u', 'a', 'e', 'i', 'o', 'u', 'u', 'u', 'n', 'n', ' ', ' ');
+    $str = str_replace($search, $replace, $str);
+    $str = preg_replace('/&(?!#[0-9]+;)/s', '', $str);
+    $search = array(' a ', ' de ', ' con ', ' por ', ' en ', ' y ', ' e ', ' o ', ' u ', ' la ', ' el ', ' lo ', ' las ', ' los ', ' fue ', ' del ', ' se ', ' su ', ' una ');
+    $str = str_replace($search, ' ', strtolower($str));
+    $str = str_replace($search, $replace, strtolower(trim($str)));
+    $str = preg_replace("/[^a-zA-Z0-9\s]/", '', $str);
+    $str = preg_replace('/\s\s+/', ' ', $str);
+    $str = str_replace(' ', '-', $str);
+    return  $str;
+}
+
+function getUrlAmigable($s){
+    $s = strtolower($s);
+    $s = preg_replace("[áàâãäª@]","a",$s);
+    $s = preg_replace("[éèêë]","e",$s);
+    $s = preg_replace("[íìîï]","i",$s);
+    $s = preg_replace("[óòôõºö]","o",$s);
+    $s = preg_replace("[úùûü]","u",$s);
+    $s = preg_replace("[ç]","c",$s);
+    $s = preg_replace("[ñ]","n",$s);
+    $s = preg_replace( "/[^a-zA-Z0-9\-]/", "-", $s );
+    $s = preg_replace( array("`[^a-z0-9]`i","`[-]+`") , "-", $s);
+
+    return trim($s, '-');
+}
+
+//GENERAR CODIGO ALEATORIO
+function CodigoAleatorio($length=10,$uc=TRUE,$n=TRUE,$sc=FALSE)
+{
+    $source = 'abcdefghijklmnopqrstuvwxyz';
+    if($uc==1) $source .= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    if($n==1) $source .= '1234567890';
+    if($sc==1) $source .= '|@#~$%()=^*+[]{}-_';
+    if($length>0){
+        $rstr = "";
+        $source = str_split($source,1);
+        for($i=1; $i<=$length; $i++){
+            mt_srand((double)microtime() * 1000000);
+            $num = mt_rand(1,count($source));
+            $rstr .= $source[$num-1];
+        }
+    }
+    return $rstr;
+}
+
+//GENERAR CODIGO ALEATORIO
+function CodigoAleatorioUpp($length=10,$n=TRUE,$sc=FALSE)
+{
+    $source = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    if($n==1) $source .= '1234567890';
+    if($sc==1) $source .= '|@#~$%()=^*+[]{}-_';
+    if($length>0){
+        $rstr = "";
+        $source = str_split($source,1);
+        for($i=1; $i<=$length; $i++){
+            mt_srand((double)microtime() * 1000000);
+            $num = mt_rand(1,count($source));
+            $rstr .= $source[$num-1];
+        }
+    }
+    return $rstr;
+}
