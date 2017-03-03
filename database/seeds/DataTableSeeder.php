d48088c51c08ba9ff4e1a0d3603aed2f73242cdc
Marco Lopez <mlopez@marostdevelopers.com>
2017-03-03 14:02:25 -0500

    sin data

--- database/seeds/DataTableSeeder.php
+++ database/seeds/DataTableSeeder.php
@@ -32,23 +32,5 @@ class DataTableSeeder extends Seeder
             ['id' => '9', 'titulo' => 'Personajes', 'slug_url' => 'personajes', 'publicar' => 1],
         ]);
 
-        //TAGS
-        DB::table('tags')->insert([
-            ['id' => '1', 'titulo' => 'Perú', 'slug_url' => 'peru', 'publicar' => 1],
-            ['id' => '2', 'titulo' => 'Facebook', 'slug_url' => 'facebook', 'publicar' => 1],
-            ['id' => '3', 'titulo' => 'Apple', 'slug_url' => 'apple', 'publicar' => 1],
-            ['id' => '4', 'titulo' => 'Bill Gates', 'slug_url' => 'bill-gates', 'publicar' => 1]
-        ]);
-
-        //NOTICIAS
-        factory(\Sucesos\Entities\Sucesos\Noticia::class, 150)->create()->each(function ($u) {
-            $u->tags()->sync([1,2,3,4]);
-        });
-
-        //COLUMNISTA
-        factory(\Sucesos\Entities\Sucesos\Columnista::class, 13)->create();
-
-        //COLUMNAS
-        factory(\Sucesos\Entities\Sucesos\Columna::class, 100)->create();
     }
 }
--- database/seeds/VideoTableSeeder.php
+++ database/seeds/VideoTableSeeder.php
@@ -11,6 +11,25 @@ class VideoTableSeeder extends Seeder
      */
     public function run()
     {
+        //TAGS
+        DB::table('tags')->insert([
+            ['id' => '1', 'titulo' => 'Perú', 'slug_url' => 'peru', 'publicar' => 1],
+            ['id' => '2', 'titulo' => 'Facebook', 'slug_url' => 'facebook', 'publicar' => 1],
+            ['id' => '3', 'titulo' => 'Apple', 'slug_url' => 'apple', 'publicar' => 1],
+            ['id' => '4', 'titulo' => 'Bill Gates', 'slug_url' => 'bill-gates', 'publicar' => 1]
+        ]);
+
+        //NOTICIAS
+        factory(\Sucesos\Entities\Sucesos\Noticia::class, 150)->create()->each(function ($u) {
+            $u->tags()->sync([1,2,3,4]);
+        });
+
+        //COLUMNISTA
+        factory(\Sucesos\Entities\Sucesos\Columnista::class, 13)->create();
+
+        //COLUMNAS
+        factory(\Sucesos\Entities\Sucesos\Columna::class, 100)->create();
+
         factory(\Sucesos\Entities\Sucesos\Video::class, 25)->create();
     }
 }
