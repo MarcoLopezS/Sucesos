7fc2290e23a65700d3031a8cb3d4a57d8d87a6fb
Marco Lopez <mlopez@marostdevelopers.com>
2017-03-04 23:24:32 -0500

    enlace a redes sociales

--- resources/views/frontend/partials/siguenos.blade.php
+++ resources/views/frontend/partials/siguenos.blade.php
@@ -1,13 +1,13 @@
 <div class="side-widget margin-bottom-50">
     <h3 class="heading-1"><span>Siguenos</span></h3>
     <div class="side-share side-share2">
-        <a class='share s_facebook'>
+        <a href="https://www.facebook.com/sucesos.pe" target="_blank" class='share s_facebook'>
             <i class="fa fa-facebook"></i>
         </a>
         <a href="https://twitter.com/sucesosperu" target="_blank" class='share s_linkedin'>
             <i class="fa fa-twitter"></i>
         </a>
-        <a class='share s_plus'>
+        <a href="https://www.youtube.com/channel/UCz25I24Wd25SyyE9FPtP_zQ" target="_blank" class='share s_plus'>
             <i class="fa fa-play"></i>
         </a>
     </div>
