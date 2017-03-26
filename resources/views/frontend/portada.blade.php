e35c0079ee86347dbc31cee0b3768996dd0369ab
Marco Lopez <mlopez@marostdevelopers.com>
2017-03-26 00:07:54 -0500

    cambio de portada

--- resources/views/frontend/portada.blade.php
+++ resources/views/frontend/portada.blade.php
@@ -35,7 +35,7 @@
                     <div class="magazine-section stackable">
                         <div class="magazine-image-details column">
                             <div class="image">
-                                <img class="fill" src="/images/portada-003.jpg">
+                                <img class="fill" src="/images/portada-004.jpg">
                             </div>
                             <div class="details">
                                 <h2>Suscríbete a Sucesos</h2>
