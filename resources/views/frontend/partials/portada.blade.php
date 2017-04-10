d98e853d62d47b8d2d52158520cc577fbeb80b70
Marco Lopez <mlopez@marostdevelopers.com>
2017-04-10 00:02:41 -0500

    portada 6

--- resources/views/frontend/partials/portada.blade.php
+++ resources/views/frontend/partials/portada.blade.php
@@ -3,7 +3,7 @@
         <h3 class="heading-1"><span>Portada</span></h3>
         <div class="layout_3--item">
             <div class="thumb" style="border: 1px solid #a2a2a2;">
-                <a href="{{ route('portada') }}"><img src="/images/portada-005.jpg" class="img-responsive" alt=""></a>
+                <a href="{{ route('portada') }}"><img src="/images/portada-006.jpg" class="img-responsive" alt=""></a>
             </div>
         </div>
     </div>
