6d0b55ba90adcb874fe374463afde1b03989c7ec
Marco Lopez <mlopez@marostdevelopers.com>
2017-04-23 23:32:49 -0500

    portada 8

--- resources/views/frontend/portada.blade.php
+++ resources/views/frontend/portada.blade.php
@@ -35,7 +35,7 @@
                     <div class="magazine-section stackable">
                         <div class="magazine-image-details column">
                             <div class="image">
-                                <img class="fill" src="/images/portada-007.jpg">
+                                <img class="fill" src="/images/portada-008.jpg">
                             </div>
                             <div class="details">
                                 <h2>Suscríbete a Sucesos</h2>
